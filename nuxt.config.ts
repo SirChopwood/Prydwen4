26b6dbcccf993339e2c5a8c0be88f14c484568d8
Ramiris <sirchopwood@gmail.com>
2026-01-08 02:53:03 +0000

    DB Fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,32 @@ export default defineNuxtConfig({
     preset: "cloudflare-durable",
     cloudflare: {
       deployConfig: true,
-      nodeCompat: true
+      nodeCompat: true,
+      wrangler: {
+        observability: {
+          enabled: true,
+        },
+        assets: {
+          directory: "./.output/public/",
+          binding: "ASSETS"
+        },
+        d1_databases: [
+          {
+            binding: 'DB',
+            database_id: '0aaa1682-dbee-49af-a1d8-68299824e654',
+            migrations_table: "_hub_migrations",
+            migrations_dir: ".output/server/db/migrations/sqlite/",
+          }
+        ],
+        durable_objects: {
+          bindings: [
+            {
+              name: "$DurableObject",
+              class_name: "$DurableObject"
+            }
+          ]
+        }
+      }
     },
     experimental: {
       openAPI: true,
@@ -40,7 +65,17 @@ export default defineNuxtConfig({
     ],
   },
   hub: {
-    db: 'sqlite'
+    db: {
+      dialect: 'sqlite',
+      driver: 'd1',
+      connection: { databaseId: '0aaa1682-dbee-49af-a1d8-68299824e654' },
+      migrationsDirs: [".output/server/db/migrations/sqlite/"],
+    },
+  },
+  $development: {
+    hub: {
+      db: "sqlite"
+    }
   },
   icon: {
     mode: 'css',
