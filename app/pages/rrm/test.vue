1db95e5fb3a077bf6dc640cec6ccd2348f58bd8b
Ramiris <sirchopwood@gmail.com>
2026-01-18 19:23:54 +0000

    WebSocket demo for testmerge

--- app/pages/rrm/test.vue
+++ app/pages/rrm/test.vue
@@ -5,11 +5,15 @@ let messages: Ref<Array<String>> = ref([])
 let ws: WebSocket | null = null
 
 onMounted(async () => {
-  ws = new WebSocket("/api/v1/rrm/events/websocket")
+})
+
+function openWs() {
+  if (ws) {return}
+  ws = new WebSocket("/api/rrm_v2/overlay")
   ws.addEventListener("open", async (event) => {
     console.log("WS Ready")
     messages.value.push(`WS Ready`)
-    ws!.send("first")
+    ws!.send(JSON.stringify({type: "ping", value: ""}))
   })
   ws.addEventListener("message", async (event) => {
     console.log(event.data)
@@ -23,7 +27,24 @@ onMounted(async () => {
     console.log(`WS Error: ${event}`)
     messages.value.push(`WS Error: ${event}`)
   })
-})
+}
+
+function testPing() {
+  ws!.send(JSON.stringify({type: "ping", value: ""}))
+}
+
+function test() {
+  ws!.send(JSON.stringify({type: "test", value: "Hello There"}))
+}
+
+function testClass() {
+  ws!.send(JSON.stringify({type: "class", value: "Ayy Lmao"}))
+}
+
+function closeWs() {
+  ws!.close()
+  ws = null
+}
 
 onUnmounted(() => {
   ws!.close()
@@ -31,8 +52,17 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="bg-neutral-800 rounded-sm border-primary border-2 m-8 flex flex-col w-full h-fit max-h-full overflow-scroll">
-    <div v-for="message of messages">{{message}}</div>
+  <div class="p-8 flex flex-col gap-8 w-full h-fit max-h-full overflow-scroll">
+    <div class="flex flex-row gap-8">
+      <button class="bg-neutral-800 rounded-sm border-primary border-2 p-2 text-3xl font-bold hover:text-primary" v-if="!ws" @click="openWs">Open</button>
+      <button class="bg-neutral-800 rounded-sm border-primary border-2 p-2 text-3xl font-bold hover:text-primary" v-if="ws" @click="testPing">Ping</button>
+      <button class="bg-neutral-800 rounded-sm border-primary border-2 p-2 text-3xl font-bold hover:text-primary" v-if="ws" @click="test">Test</button>
+      <button class="bg-neutral-800 rounded-sm border-primary border-2 p-2 text-3xl font-bold hover:text-primary" v-if="ws" @click="testClass">Class</button>
+      <button class="bg-neutral-800 rounded-sm border-primary border-2 p-2 text-3xl font-bold hover:text-primary" v-if="ws" @click="closeWs">Close</button>
+    </div>
+    <div class="bg-neutral-800 rounded-sm border-primary border-2 flex flex-col w-full h-fit max-h-full overflow-scroll">
+      <div v-for="message of messages">{{message}}</div>
+    </div>
   </div>
 </template>
 
--- server/api/rrm_v2/overlay.ts
+++ server/api/rrm_v2/overlay.ts
@@ -3,12 +3,19 @@ import {Peer} from "crossws";
 export default defineWebSocketHandler({
     open(peer) {
         peer.send({type: "connection", value: "Open"})
+        peer.context.class = new TestClass(peer)
     },
 
     async message(peer, message) {
-        let msg: {type: string, value: any} = message.json()
+        let msg: { type: string, value: any }
+        try {
+            msg = message.json()
+        } catch (e) {
+            console.log(`Unknown message: ${message.text()}`)
+            return
+        }
         if (!msg.type) {
-            console.log(`Unknown message: ${msg}`)
+            console.log(`Unknown message type: ${msg}`)
             return
         }
 
@@ -19,7 +26,11 @@ export default defineWebSocketHandler({
                 break
             case "test":
                 setInterval(updateSession, 5000, peer, msg.value)
-                peer.send({ type: "test", message: "Set" })
+                peer.send({ type: "test", value: "Set" })
+                break
+            case "class":
+                // @ts-ignore
+                await peer.context.class.testFunction()
                 break
             default:
                 console.log(`Unknown type: ${msg.type}`)
@@ -34,9 +45,23 @@ export default defineWebSocketHandler({
 })
 
 async function pingTest(peer: Peer) {
-    peer.send({ type: "ping", message: "pong" })
+    peer.send({ type: "ping", value: "pong" })
 }
 
 async function updateSession(peer: Peer, msg: any) {
-    peer.send({ type: "test", message: `${msg} - ${ new Date().toUTCString() }` })
+    peer.send({ type: "test", value: `${msg} - ${ new Date().toUTCString() }` })
+}
+
+class TestClass {
+    randomNumber: number = Math.floor(Math.random() * 100)
+    peer: Peer
+
+    constructor(peer: Peer) {
+        this.peer = peer
+    }
+
+    async testFunction() {
+        console.log(this.randomNumber)
+        this.peer.send({ type: "class", value: String(this.randomNumber) })
+    }
 }
