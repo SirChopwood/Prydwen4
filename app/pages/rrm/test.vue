1033fac4657acb9c94a22c142202e0c4bbc132ee
Ramiris <sirchopwood@gmail.com>
2026-01-23 06:25:05 +0000

    Revert "WebSocket Testmerge (#1)" (#2)
    This reverts commit 3bf2327ca883728aa34ea2e142a380fe3d3ed8f1.

--- app/pages/rrm/test.vue
+++ app/pages/rrm/test.vue
@@ -5,15 +5,11 @@ let messages: Ref<Array<String>> = ref([])
 let ws: WebSocket | null = null
 
 onMounted(async () => {
-})
-
-function openWs() {
-  if (ws) {return}
-  ws = new WebSocket("/api/rrm_v2/overlay")
+  ws = new WebSocket("/api/v1/rrm/events/websocket")
   ws.addEventListener("open", async (event) => {
     console.log("WS Ready")
     messages.value.push(`WS Ready`)
-    ws!.send(JSON.stringify({type: "ping", value: ""}))
+    ws!.send("first")
   })
   ws.addEventListener("message", async (event) => {
     console.log(event.data)
@@ -27,24 +23,7 @@ function openWs() {
     console.log(`WS Error: ${event}`)
     messages.value.push(`WS Error: ${event}`)
   })
-}
-
-function testPing() {
-  ws!.send(JSON.stringify({type: "ping", value: ""}))
-}
-
-function test() {
-  ws!.send(JSON.stringify({type: "test", value: "Hello There"}))
-}
-
-function testClass() {
-  ws!.send(JSON.stringify({type: "class", value: "Ayy Lmao"}))
-}
-
-function closeWs() {
-  ws!.close()
-  ws = null
-}
+})
 
 onUnmounted(() => {
   ws!.close()
@@ -52,17 +31,8 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="p-8 flex flex-col gap-8 w-full h-fit max-h-full overflow-scroll">
-    <div class="flex flex-row gap-8">
-      <button class="bg-neutral-800 rounded-sm border-primary border-2 p-2 text-3xl font-bold hover:text-primary" v-if="!ws" @click="openWs">Open</button>
-      <button class="bg-neutral-800 rounded-sm border-primary border-2 p-2 text-3xl font-bold hover:text-primary" v-if="ws" @click="testPing">Ping</button>
-      <button class="bg-neutral-800 rounded-sm border-primary border-2 p-2 text-3xl font-bold hover:text-primary" v-if="ws" @click="test">Test</button>
-      <button class="bg-neutral-800 rounded-sm border-primary border-2 p-2 text-3xl font-bold hover:text-primary" v-if="ws" @click="testClass">Class</button>
-      <button class="bg-neutral-800 rounded-sm border-primary border-2 p-2 text-3xl font-bold hover:text-primary" v-if="ws" @click="closeWs">Close</button>
-    </div>
-    <div class="bg-neutral-800 rounded-sm border-primary border-2 flex flex-col w-full h-fit max-h-full overflow-scroll">
-      <div v-for="message of messages">{{message}}</div>
-    </div>
+  <div class="bg-neutral-800 rounded-sm border-primary border-2 m-8 flex flex-col w-full h-fit max-h-full overflow-scroll">
+    <div v-for="message of messages">{{message}}</div>
   </div>
 </template>
 
