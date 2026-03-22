26cdb595596d590ae49851ec1f341d42a4667743
Ramiris <sirchopwood@gmail.com>
2026-03-22 20:22:47 +0000

    Remove SSR/Prerender from RRM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -92,7 +92,7 @@ export default defineNuxtConfig({
   },
   routeRules: {
     '.**': { prerender: false },
-    "/rrm_v2": {prerender: false, ssr: false},
+    "/rrm_v2/**": {prerender: false, ssr: false},
     '/rrm/**': { redirect: { to: '/rrm_v2/**', statusCode: 301 } },
   }
 })
