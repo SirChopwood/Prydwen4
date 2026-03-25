256fc43760bbde28dfbaf30a6e4bc19552f5c8d9
Ramiris <sirchopwood@gmail.com>
2026-03-25 20:04:29 +0000

    Add string responses to add request if it fails.

--- server/api/rrm_v2/request/add.post.ts
+++ server/api/rrm_v2/request/add.post.ts
@@ -14,34 +14,35 @@ export default defineEventHandler(async (event) => {
 
     let results: Array<typeof schema.RRM_V2_Requests.$inferSelect> = []
     let session = await fetchSession(context.body.sessionId)
-    if (session) {
-        if (!context.body.force && session.requestState === "Locked") {
-            return results
-        }
-        for (let request of context.body.codes) {
-            for (let sourceName of session.sources) {
-                let result
-                try {
-                    result = await Sources[String(sourceName)]!(request)
-                } catch (e) {
-                    console.log(`Failed to process ${request} as ${sourceName}, with Error: ${e}`)
-                    continue
-                }
-                if (result) {
-                    console.log(`Processed ${request} as ${sourceName}.`)
+    if (!session) {
+        return "There is no session open right now, please try again later."
+    }
+    if (!context.body.force && session.requestState === "Locked") {
+        return "Queue is locked, please wait for the DJ to unlock it."
+    }
+    for (let request of context.body.codes) {
+        for (let sourceName of session.sources) {
+            let result
+            try {
+                result = await Sources[String(sourceName)]!(request)
+            } catch (e) {
+                console.log(`Failed to process ${request} as ${sourceName}, with Error: ${e}`)
+                continue
+            }
+            if (result) {
+                console.log(`Processed ${request} as ${sourceName}.`)
 
-                    // ADD IN LOGIC FOR DUPLICATE SONGS (Ignore if Force enabled)
+                // ADD IN LOGIC FOR DUPLICATE SONGS (Ignore if Force enabled)
 
-                    let success = await createRequest(context.body.sessionId, context.body.user, result)
-                    if (success) {
-                        results.push(success)
-                        break
-                    }
-                } else {
-                    console.log(`Failed to process ${request} as ${sourceName}.`)
+                let success = await createRequest(context.body.sessionId, context.body.user, result)
+                if (success) {
+                    results.push(success)
+                    break
                 }
+            } else {
+                console.log(`Failed to process ${request} as ${sourceName}.`)
             }
         }
-        return results
     }
+    return results
 })
